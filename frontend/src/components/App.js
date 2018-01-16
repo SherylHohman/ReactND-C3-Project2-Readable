ab9ca0746378eff807680fc4cc96813810e0b67c
SherylHohman <SherylHohman@users.noreply.github.com>
2018-01-16 01:17:17 -0800

    feat: replace default render, styles, and UI, mvp

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import logo from '../logo.svg';
-import '../App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="app-container">
+        <header className="app-header">
+          <h1 className="app-title">Readable</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p className="app-intro">
+          ..an app for posting and viewing posts and comments
         </p>
       </div>
     );
