ed4b0e9032473cb4768f31b18e72dd834b231fe4
SherylHohman <SherylHohman@users.noreply.github.com>
2018-01-16 00:28:28 -0800

    squash commit: react-redux app setup, mvp render
    refactor: organize app folder/file structure
    
    chore: yarn add redux react-redux
    
    feat: add: redux and skeleton react-redux bindings
    
        also includes code for Chrome react dev tools
    
    feat: replace default render, styles, and UI, mvp
    
    ..Also testing rebase -i to squash commits *just because*
        (for the experience). and to see how it affects merges
        between branches.

---
+++ frontend/src/components/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app-container">
+        <header className="app-header">
+          <h1 className="app-title">Readable</h1>
+        </header>
+        <p className="app-intro">
+          ..an app for posting and viewing posts and comments
+        </p>
+      </div>
+    );
+  }
+}
+
+function mapDispatchToProps(dispatch){
+  return ({
+
+  })
+}
+
+function mapStoreToProps ( {  }) {
+  return {
+
+  }
+};
+
+export default connect(mapStoreToProps, mapDispatchToProps)(App);
+
