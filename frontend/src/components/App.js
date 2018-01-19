539c2944ba1bc93427a286cde4edbe8ac8c7700d
SherylHohman <SherylHohman@users.noreply.github.com>
2018-01-19 12:46:26 -0800

    feat: api request data into App; nieve data render
    	OK! I now know how to
    		- write an api request,
    		- return the api data from the request, to App.js
    		- interpret and transform api data into state-like data (arrays)
    	And I put a nieve render of categories and posts onto the page
    
    	TODO: put api data into store, and
    		  connect and transform store data into state-like data format
    		  (ie react likes arrays to map over; store prefers objects)

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -6,16 +6,35 @@ class App extends Component {
 
   componentWillMount() {
     console.log("in componentWillMount");
-    fetchCategories().then((data) => {
-      console.log('got lost in finding my way back');
-      console.log('data', data);
-      // this.setState({ 'categories': ['a', 'b', 'c'] });
-    fetchPosts();
+
+    fetchCategories().then((categoryObjects) => {
+
+      // console.log('categories fetched:', categoryObjects);
+      const categories = categoryObjects
+        .reduce((acc, categoryObject) => {
+          return acc.concat(categoryObject.name);
+        }, []);
+      console.log('categories as array;', categories);
+
+      this.setState({ categories });
+    });
+
+    fetchPosts().then((postObjects) => {
+      console.log('posts fetched:', postObjects);
+
+      const posts = postObjects
+        .reduce((acc, postObject) => {
+          return acc.concat(postObject);
+        }, []);
+      console.log('posts as array;', posts);
+
+      this.setState({ posts });
     });
   }
 
   state: {
-    categories: "nothing here !",
+    categories: "fetching categories..",
+    posts: "fetching posts.."
   }
 
   render() {
@@ -24,17 +43,57 @@ class App extends Component {
       console.log('this.state.categories', this.state.categories);
     }
     else{
-      console.log('no state!');
+      console.log('no state! - use setStoreToProps, or setState');
+    }
+    if ((this.state) && (this.state.posts)) {
+      console.log('this.state.posts', this.state.posts);
+    }
+    else{
+      console.log('no state! - use setStoreToProps, or setState');
     }
 
     return (
       <div className="app-container">
         <header className="app-header">
           <h1 className="app-title">Readable</h1>
+          {this.state && this.state.categories &&
+              (
+                <ul>
+                  {this.state.categories.map(category => {
+                    return (
+                      <li key={category}>{category}</li>
+                    )
+                  })}
+                </ul>
+              )
+          }
+          { !this.state &&
+            // !this.state.categories &&
+            <p>No Categories Available</p>
+          }
         </header>
-        <p className="app-intro">
+
+        <div className="app-intro">
           ..an app for posting and viewing posts and comments
-        </p>
+          <hr />
+          {this.state && this.state.posts &&
+              (
+                <ul>
+                  {this.state.posts.map(post => {
+                    return (
+                      <li key={post.id}>title:{post.title}</li>
+                    )
+                  })}
+                </ul>
+              )
+          }
+          { !this.state &&
+            // !this.state.posts &&
+            <p>No Categories Available</p>
+          }
+          <hr />
+        </div>
+
       </div>
     );
   }
--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,60 +1,34 @@
-const AUTH_HEADER = {'Authorization': 'sheryl-readable-authorization'};
-// const AUTH_HEADER = 'sheryl-readable-authorization';
+const api = "http://localhost:3001"
 
-// export function fetchRecipes (food = '') {
-//   food = food.trim()
-
-//   return fetch(`https://api.edamam.com/search?q=${food}&app_id=${API_ID}&app_key=${APP_KEY}`)
-//     .then((res) => res.json())
-//     .then(({ hits }) => hits.map(({ recipe }) => recipe))
-// }
 let token = localStorage.token
 if (!token)
   token = localStorage.token = Math.random().toString(36).substr(-8)
 
 const headers = {
-  'Accept': 'application/json',
-  'Authorization': token
+  'Content-Type': 'application/json',  // for server
+  'Authorization': token,
+  'Accept': 'application/json',  // for response!
 }
-
-const api = "http://localhost:3001"
-const route = '/categories';
-const url = api + route;
 
 export const fetchCategories = () =>
-fetch(`${api}/categories`, { headers })
-.then((res) => {
-    return res.json()
-})
-.then((data) => {
-   console.log('data.categoties', data.categoties);
-   return data.categories;
-});
+  fetch(`${api}/categories`, { headers })
+  .then((res) => {
+      const parsed = res.json();
+      // console.log('res:', res, '\nparsed:', parsed);
+      return parsed;
+      // return res.json();
+  })
+  .then((data) => {
+     // console.log('data.categories', data.categories);
+     return data.categories;
+  });
 
-
-// export const fetchCategories = () => {
-//   // console.log('in fetchCategories');
-//   const url = '/categories';
-//   // return (
-//     fetch(url, {
-//       headers,
-//       // method: 'GET',
-//     })
-//     .then((response) => {
-//       // console.log('got response..\n', response);
-//       console.log('got response..\n', response.json());
-//       return response.json()
-//     })
-//     .then(data => data.categories)
-// }
-
-export const fetchPosts = () => {
-  // console.log('in fetchPosts');
-  // const url = '/posts';
-  // return (
-  //   fetch(url, {headers: AUTH_HEADER})
-  //   .then((response) => {
-  //     console.log('got response..\n', response);
-  //     return response.json()})
-  // );
-}
+export const fetchPosts = () =>
+  fetch(`${api}/posts`, { headers })
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+     console.log('posts data :', data);
+     return data;
+  })
