d406c6999bbe8ce8b4c2204ba6fb2c7ae04f677a
SherylHohman <SherylHohman@users.noreply.github.com>
2018-01-20 06:40:51 -0800

    feat: proper refactor of Categories using redux store
    	TODO: separate action creators to actions.js file,
    			or rename reducers to ducks.js
    	Next: refactor Posts properly in the same vain.

--- frontend/src/components/Categories.js
+++ frontend/src/components/Categories.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchCategories } from '../utils/api';
+import { fetchCategories } from '../state/categories/reducers';
+  // TODO:
+  //  `reducers.js` is improperly named. Im actually importing an action creator, normally in an actions file. I have a quasi duck file, that should be named 'ducks' or 'actionsReducers', or segregated into actions and reducers files.
+// import { fetchCategories } from '../utils/api';
 
 export class Categories extends Component {
 
   componentWillMount() {
     // console.log("in Categories componentWillMount");
 
-    fetchCategories().then((categories) => {
-      // console.log('cDM|fetchCategories: categories as array of objects, with extraneous getRequest properties: ', categories)
-      // this.setState({ categories });
-    });
-    // console.log('cDM, leaving fetchCategories:', `${this.state||this.props||'no state or props'}`);
+
+
+    // fetchCategories().then((categories) => {
+    //   // console.log('cDM|fetchCategories: categories as array of objects, with extraneous getRequest properties: ', categories)
+    //   // this.setState({ categories });
+    // });
+    // // console.log('cDM, leaving fetchCategories:', `${this.state||this.props||'no state or props'}`);
   }
 
   render() {
@@ -49,7 +54,7 @@ export class Categories extends Component {
 // export default Categories
 function mapDispatchToProps(dispatch){
   return ({
-
+    getCategories: () => dispatch(fetchCategories()),
   })
 }
 
--- frontend/src/state/categories/reducers.js
+++ frontend/src/state/categories/reducers.js
@@ -1,14 +1,62 @@
+import * as ReaderAPI from '../../utils/api';
+
 // ACTION TYPES
-  export const GET_CATEGORIES = 'GET_CATEGORIES';
+  export const FETCH_CATEGORIES = 'FETCH_CATEGORIES';
+  export const FETCH_CATEGORIES_SUCCESS = 'FETCH_CATEGORIES_SUCCESS';
+  export const FETCH_CATEGORIES_FAILURE = 'FETCH_CATEGORIES_FAILURE';
   export const SET_CURRENT_CATEGORY = 'SET_CURRENT_CATEGORY';
 
-// ACTION CREATORS
-  export function getCategories({ categories={} }){
-    return({
-      type: GET_CATEGORIES,
-      categories,
+// THUNK ACTION CREATORS (allows side effects: api calls)
+  //  library automatically calls the returned functions with
+  //    dispatch as the 1st argument (saves typing)
+  //  automatically 'getState' is automatically passed into these
+  //    (thunk action creators)
+  //    as a 2nd param, so state is automatically available inside
+  //    thunk action creators.
+  //
+
+// FAT ACTION CREATORS (business logic, then call dispatch on results)
+  //  downside: no access to global state.
+  //  So if global state is needed to make business logic decision with,
+  //  it needs to be passed through components (including intermediary
+  //    components)
+  //  in that case: refactor to use redux-thunks instead (THUNK ACTION CREATOR)
+  export function fetchCategories(dispatch){
+    dispatch({
+      type: FETCH_CATEGORIES,
     });
-  }
+    ReaderAPI.fetchCategories()
+      .then(categories =>
+        dispatch({
+          type: FETCH_CATEGORIES_SUCCESS,
+          categories,
+        })
+      )
+      .catch(err => {
+        console.error(err);  //  in case of render error
+        dispatch({
+          type: FETCH_CATEGORIES_FAILURE,
+          err,
+          error: true,
+        })
+      });
+  };
+
+
+// ACTION CREATORS
+  // export function fetchCategories({ categories={} }){
+  //   return({
+  //     type: FETCH_CATEGORIES,
+  //     categories,
+  //   });
+  // }
+
+  // export function fetchCategoriesSuccess({ categories={} }){
+  //   return({
+  //     type: FETCH_CATEGORIES_SUCCESS,
+  //     categories,
+  //   });
+  // }
 
   export function setCurrentCategory({ selectedCategory=null }) {
     // used to filter all posts by category
@@ -35,15 +83,26 @@
 // REDUCER(s)
   function category(state=sampleData, action){
     switch (action.type){
-      case GET_CATEGORIES:
+      case FETCH_CATEGORIES_SUCCESS:
+        // update state with list of category names
         return ({
           ...state,
           categories: action.categories
         });
+      case FETCH_CATEGORIES:
+        return ({
+          ...state,
+          //  could set an loading (boolean flag) to some state that would show a loading message
+        });
+      case FETCH_CATEGORIES_FAILURE:
+        return ({
+          ...state,
+          //  could set an error message on some state to handle errors
+        });
       case SET_CURRENT_CATEGORY:
         return({
           ...state,
-          selectedCategory: action.selectedCategory,
+          // selectedCategory: action.selectedCategory,
         });
       default:
         return state;
@@ -51,14 +110,4 @@
   }
 
 export default category;
-
-
-
-/* TODO: ?
-  To avoid falling into the "getters" setters" way of thinking,
-  - Perhaps rename getCategories, to fetchCategories,
-   indicating that we are calling an API ??
-  - Perhaps setCurrentCategory might be selectedCategory ??
-  Resolve this same paradigm for comments, posts, (user), etc., if necessary.
-*/
 
