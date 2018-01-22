07e4b0932c4b67b3b923c27cabdf52bfea380bc2
SherylHohman <SherylHohman@users.noreply.github.com>
2018-01-22 01:07:55 -0800

    bug: yarn REMOVE react-router-redux, index.js changes
    	Don't need react-router-redux. Was following an outdated blog post.
    	index.js has been updated for adding Router, in addition to removing
    		code relating to the uneeded package.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-// import { combineReducers } from 'redux';
-// import * as reducers from "./state/index.js";     // might rename folder to `store`
 import rootReducer from './state/index.js';
 import App from './components/App';
 import './index.css';
@@ -20,12 +19,15 @@ const store = createStore(
   )
 );
 
-
 ReactDOM.render(
   <Provider store={store}>
-    <App/>
+    <BrowserRouter><App/></BrowserRouter>
   </Provider>,
   document.getElementById('root')
 )
 
 registerServiceWorker();
+
+
+// TODO: not using thunk: refactor without useing:
+// DEVTOOLS...COMPOSE, composeEnhancers, compose, or applyMiddleware
