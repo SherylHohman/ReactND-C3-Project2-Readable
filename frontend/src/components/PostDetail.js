b724ed00e11982b351a9bba65d62b37667874f06
SherylHohman <SherylHohman@users.noreply.github.com>
2018-01-23 08:45:00 -0800

    refactor: PostDetail to class component. No Help!
    	*Still* No Props data is getting passed in from Link

--- frontend/src/components/PostDetail.js
+++ frontend/src/components/PostDetail.js
@@ -8,73 +8,119 @@ import { Route } from 'react-router-dom';  // TODO: DELETE - TEMP for
 // import { connect } from 'react-redux';
 // import { fetchPosts } from '../state/posts/ducks';
 
-const PostDetail = function(props) {
-  console.log('in PostDetail');
-  // const post = props.post;
-  // console.log('props:', props.post);
+// ----------------------------------------
 
-  const post =
-    (props && props.post) ||
-    (props && props.location && props.location.state && props.location.state.post) ||
-    (props && props.match && props.match.params && props.match.params.post) ||
-    null;
-  // props post accessed by query string
-  // https://github.com/ReactTraining/react-router/issues/4036
+// const PostDetail = function(props) {
+  // console.log('in PostDetail');
+  // // const post = props.post;
+  // // console.log('props:', props.post);
 
-  // const { post } = props.location.state;
-  // // prop passed in from a Link component
-  // // https://stackoverflow.com/a/45599159/5411817
+  // const post =
+  //   (props && props.post) ||
+  //   (props && props.location && props.location.state && props.location.state.post) ||
+  //   (props && props.match && props.match.params && props.match.params.post) ||
+  //   null;
+  // // props post accessed by query string
+  // // https://github.com/ReactTraining/react-router/issues/4036
 
-  // TODO if not using store, and page is loaded from saved url,
-  //  fetch the post, based on post.id that's in the url
-  // TODO "connect" this component, and post isn't in store, fetch it.
+  // // const { post } = props.location.state;
+  // // // prop passed in from a Link component
+  // // // https://stackoverflow.com/a/45599159/5411817
 
-  if (!props){console.log('props is undefined');} else {console.log('props:', props);}
-  if (post !== null) {console.log('props', props, 'in PostDetail from Link in Posts')} else {console.log('post is null');}
-  if (props && props.match) {console.log('match', props.match);}
+  // // TODO if not using store, and page is loaded from saved url,
+  // //  fetch the post, based on post.id that's in the url
+  // // TODO "connect" this component, and post isn't in store, fetch it.
 
-  if (post === null) return (<div>post wasn't present in props</div>)
+  // if (!props){console.log('props is undefined');} else {console.log('props:', props);}
+  // if (post !== null) {console.log('props', props, 'in PostDetail from Link in Posts')} else {console.log('post is null');}
+  // if (props && props.match) {console.log('match', props.match);}
 
-  return (
-    <div>
-            <div> -- Post (Detail) Page: {post.body} -- </div>
-            <Comments post={post} />
-    </div>
-  );
+  // if (post === null) return (<div>post wasn't present in props</div>)
 
-  // return  (
+  // return (
   //   <div>
-  //     <PostHeader post={post} />
-  //     <div>post.body</div>
-  //     <Comments post={post} />
+  //           <div> -- Post (Detail) Page: {post.body} -- </div>
+  //           <Comments post={post} />
   //   </div>
-  // )
+  // );
 
-};
+  // // return  (
+  // //   <div>
+  // //     <PostHeader post={post} />
+  // //     <div>post.body</div>
+  // //     <Comments post={post} />
+  // //   </div>
+  // // )
+
+// };
 
 // PostDetail.propTypes = {
 //   post: PropTypes.object.isRequired,
 // }
 
+// ----------------------------------------
+export class PostDetail extends Component {
 
-// export class PostDetail extends Component {
+  render(){
 
-//   render(){
+    console.log('in PostDetail');
 
-//   }
+    const props = this.props;
+    if (!props){console.log('props is undefined');} else {console.log('props:', props);}
+
+    const post =
+      (props && props.post) ||
+      (props && props.location && props.location.state && props.location.state.post) ||
+      (props && props.match && props.match.params && props.match.params.post) ||
+      null;
+
+    // props post accessed by query string
+    // https://github.com/ReactTraining/react-router/issues/4036
+
+    // const { post } = props.location.state;
+    // // prop passed in from a Link component
+    // // https://stackoverflow.com/a/45599159/5411817
+
+    // TODO if not using store, and page is loaded from saved url,
+    //  fetch the post, based on post.id that's in the url
+    // TODO "connect" this component, and post isn't in store, fetch it.
+
+    if (post !== null) {console.log('props', props, 'in PostDetail from Link in Posts')} else {console.log('post is null');}
+    if (props && props.match) {console.log('match', props.match);}
+
+    if (post === null) return (<div>post wasn't present in props:</div>)
+
+    return (
+      <div>
+              <div> -- Post (Detail) Page: {post.body} -- </div>
+              <Comments post={post} />
+      </div>
+    );
+
+    // return  (
+    //   <div>
+    //     <PostHeader post={post} />
+    //     <div>post.body</div>
+    //     <Comments post={post} />
+    //   </div>
+    // )
+
+
+  }
+}
+// ----------------------------------------
+
+// function mapDispatchToProps(dispatch){
+//   return ({
+
+//   })
 // }
 
-function mapDispatchToProps(dispatch){
-  return ({
-
-  })
-}
-
-function mapStoreToProps ( store ) {
-  return {
-    ...store,
-  }
-};
+// function mapStoreToProps ( store ) {
+//   return {
+//     ...store,
+//   }
+// };
 
 // export default connect(mapStoreToProps, mapDispatchToProps)(PostDetail);
 
