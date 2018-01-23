e101c9f444d7d9cdb3d83ac744a5226cde4afcb5
SherylHohman <SherylHohman@users.noreply.github.com>
2018-01-23 08:11:58 -0800

    bug: fix assignment, compiles now, still No Props!
    	fixed silly vexing bug in assignment statement (forgot the "= props") part.
    	Still, though, cannot seem to get Anything passed from Link into the
    		props of component that I'm linking to !! WHY ?

--- frontend/src/components/PostDetail.js
+++ frontend/src/components/PostDetail.js
@@ -3,37 +3,58 @@ import React, { Component } from 'react';
 import PostHeader from './PostHeader';
 import Comments from './Comments';
 import PropTypes from 'prop-types';
+import { Route } from 'react-router-dom';  // TODO: DELETE - TEMP for
 
 // import { connect } from 'react-redux';
 // import { fetchPosts } from '../state/posts/ducks';
 
 const PostDetail = function(props) {
+  console.log('in PostDetail');
   // const post = props.post;
-  console.log('props', props, 'in PostDetail from Link in Posts');
+  // console.log('props:', props.post);
+
+  const post =
+    (props && props.post) ||
+    (props && props.location && props.location.state && props.location.state.post) ||
+    (props && props.match && props.match.params && props.match.params.post) ||
+    null;
+  // props post accessed by query string
+  // https://github.com/ReactTraining/react-router/issues/4036
 
   // const { post } = props.location.state;
-  // // props post accessed by query string
-  // // https://github.com/ReactTraining/react-router/issues/4036
+  // // prop passed in from a Link component
+  // // https://stackoverflow.com/a/45599159/5411817
 
-  const { post } = props.location.state;
-  // prop passed in from a Link component
-  // https://stackoverflow.com/a/45599159/5411817
+  // TODO if not using store, and page is loaded from saved url,
+  //  fetch the post, based on post.id that's in the url
+  // TODO "connect" this component, and post isn't in store, fetch it.
 
+  if (!props){console.log('props is undefined');} else {console.log('props:', props);}
+  if (post !== null) {console.log('props', props, 'in PostDetail from Link in Posts')} else {console.log('post is null');}
+  if (props && props.match) {console.log('match', props.match);}
 
-  console.log('props:', props);
+  if (post === null) return (<div>post wasn't present in props</div>)
 
-  return  (
+  return (
     <div>
-      <PostHeader post={post} />
-      <div>post.body</div>
-      <Comments post={post} />
+            <div> -- Post (Detail) Page: {post.body} -- </div>
+            <Comments post={post} />
     </div>
-  )
+  );
+
+  // return  (
+  //   <div>
+  //     <PostHeader post={post} />
+  //     <div>post.body</div>
+  //     <Comments post={post} />
+  //   </div>
+  // )
+
 };
 
-PostHeader.propTypes = {
-  post:PropTypes.object.isRequired,
-}
+// PostDetail.propTypes = {
+//   post: PropTypes.object.isRequired,
+// }
 
 
 // export class PostDetail extends Component {
@@ -43,17 +64,17 @@ PostHeader.propTypes = {
 //   }
 // }
 
-// function mapDispatchToProps(dispatch){
-//   return ({
+function mapDispatchToProps(dispatch){
+  return ({
 
-//   })
-// }
+  })
+}
 
-// function mapStoreToProps ( store ) {
-//   return {
-//     ...store,
-//   }
-// };
+function mapStoreToProps ( store ) {
+  return {
+    ...store,
+  }
+};
 
 // export default connect(mapStoreToProps, mapDispatchToProps)(PostDetail);
 
--- frontend/src/components/PostHeader.js
+++ frontend/src/components/PostHeader.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 // import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { fetchPosts } from '../state/posts/ducks';
+import { Route } from 'react-router-dom';  // TODO: DELETE - TEMP for
+import PostDetail from './PostDetail';  // TODO: DELETE - TEMP for debugging.
 import PropTypes from 'prop-types';
+
 
 const PostHeader = function(props) {
   const post = props.post;
@@ -14,38 +17,56 @@ const PostHeader = function(props) {
   //  (maybe, maybe not on Home Page, listing of all posts)
   const {author, timestamp} = post;
 
-  if (post.deleted) {
-    // if user accesses this page through an old URL..
-    console.log('Can\'t render Postheader of deleted post id:', post.id);
-    return (
-      <div><h2>This Post has been Deleted</h2></div>
-    )
-  };
+  // if (post.deleted) {
+  //   // if user accesses this page through an old URL..
+  //   console.log('Can\'t render Postheader of deleted post id:', post.id);
+  //   return (
+  //     <div><h2>This Post has been Deleted</h2></div>
+  //   )
+  // };
 
   // may want the path to be in this format: `path="/task/?:taskId?"`
-  const toURLaAndPostId = {
+  const location = {
             pathname: `/post/?:${post.id}`,
             query: {id: post.id},
             state: { post: props.post }
           }
 
-  // const toURLaAndPostId = {
+  // const location = {
   //           pathname: '/post',
   //           query: {id: post.id},
   //           state: { post: props.post }
   //         }
   // https://github.com/ReactTraining/react-router/issues/4036
 
-  // const toURLaAndPostId = {
+  // const location = {
   //           pathname: `/post/${post.id}/`,
   //           state: { post: props.post }
   //         }
+
+  // return  (
+  //       /*temp insert PostDetail component directly*/
+  //       <div>
+  //         <PostDetail post={post} />
+  //       </div>
+  //       /*end temp insert PostDetail component directly*/
+  // )
+
+  // return  (
+  //       /*temp insert PostDetail component directly*/
+  //       <div>
+  //         <h2>{title}</h2>
+  //         <PostDetail post={post} />
+  //       </div>
+  //       /*end temp insert PostDetail component directly*/
+  // )
+
 
   return  (
     <div>
       <div key={post.id}>
 
-        <Link to={`/post/${post.id}/`} >
+        <Link to={location} >
           <h2>{title}</h2>
         </Link>
 
@@ -56,12 +77,49 @@ const PostHeader = function(props) {
       </div>
     </div>
   )
+
+
+    // return  (
+  //   <div>
+  //     <div key={post.id}>
+
+  //       <Link to={`/post/${post.id}/`} >
+  //         <h2>{title}</h2>
+  //       </Link>
+
+  //       <div>votes: {voteScore} increment decrement</div>
+  //       <p>{category}</p>
+  //       <div>number of comments: {commentCount}</div>
+  //       <hr />
+  //     </div>
+  //   </div>
+  // )
+
+
+  // return  (
+  //   <div>
+  //     <div key={post.id}>
+
+  //       <Link to={`/post/${post.id}/`} >
+  //         <h2>{title}</h2>
+  //       </Link>
+
+  //       <div>votes: {voteScore} increment decrement</div>
+  //       <p>{category}</p>
+  //       <div>number of comments: {commentCount}</div>
+  //       <hr />
+  //     </div>
+  //   </div>
+  // )
+
 };
 
-PostHeader.propTypes = {
-  // post: PropTypes.object.isRequired,       // if keep as a "dump" component
+{/*PostHeader.propTypes = {
+  post: PropTypes.object.isRequired,       // if keep as a "dumb" component
   // postId: PropTypes.string.isRequired,  // if turn into a connected comp
 }
+
+*/}
 
 
 // export class PostHeader extends Component {
