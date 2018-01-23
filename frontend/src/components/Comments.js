f497607b5850dd01bf0586c1bb87f20de5612ef7
SherylHohman <SherylHohman@users.noreply.github.com>
2018-01-23 10:51:15 -0800

    refactor Comments into class component 	why: Needs componentWillMount to make anAPI request to get the comments

--- frontend/src/components/Comments.js
+++ frontend/src/components/Comments.js
@@ -4,39 +4,37 @@ import PropTypes from 'prop-types';
 // import { connect } from 'react-redux';
 // import { fetchPosts } from '../state/posts/ducks';
 
-const Comments = function(props) {
-  const { post } = props;
-  console.log('props:', props, 'in Comments Component');
 
-  //  TODO: need to know if post was deleted or not.
-  //    do I send in the entire post, or just post.id, post.deleted
+export class Comments extends Component {
 
-  if (post.deleted) {
-    return (
-      <div>
-        <h3> ..Oops! This post has been deleted.</h3>
-        <p>No comments to show.</p>
-      </div>
+  static propTypes = {
+    post:PropTypes.object.isRequired,
+  }
+
+  render(){
+    const { post } = this.props;
+    console.log('this.props:', this.props, 'in Comments Component');
+
+    //  TODO: need to know if post was deleted or not.
+    //    do I pass in the entire post, or just post.id and post.deleted
+
+    if (post.deleted) {
+      return (
+        <div>
+          <h3> ..Oops! This post has been deleted.</h3>
+          <p>No comments to show.</p>
+        </div>
+      )
+    }
+
+    return  (
+
+      <p> ---Comments Component--- </p>
+      // TODO: only render undeleted comments
     )
   }
 
-  return  (
-
-    <p> ---Comments Component--- </p>
-    // TODO: only render undeleted comments
-  )
-};
-
-Comments.propTypes = {
-  post:PropTypes.object.isRequired,
 }
-
-// export class Comments extends Component {
-
-//   render(){
-
-//   }
-// }
 
 // function mapDispatchToProps(dispatch){
 //   return ({
