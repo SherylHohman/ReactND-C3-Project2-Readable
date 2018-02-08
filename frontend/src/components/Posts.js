e65ad5d3dfc100d88c89cb0cbb93d255a1f91727
Sheryl Hohman <sherylhohman@yahoo.com>
2018-02-08 01:29:59 -0800

    refactor: style: add HOME.category fallback default
        mostly style: split code onto multiple lines, add comments
        also: add default fallback value of Home.category.name in mapStoreToProps

--- frontend/src/components/Posts.js
+++ frontend/src/components/Posts.js
@@ -3,18 +3,20 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { fetchPosts } from '../store/posts';
 import Categories from './Categories';
-import { changeView } from '../store/viewData';
+import { changeView, HOME } from '../store/viewData';
 
 export class Posts extends Component {
 
   componentDidMount() {
-    this.props.fetchPosts(this.props.categoryName);
-    console.log('Posts componentDidMount ..fetching, posts for category:', this.props.categoryName);
+    this.props.fetchPosts(this.props.selectedCategoryName);
+    // console.log('Posts componentDidMount ..fetching, posts for category:', this.props.selectedCategoryName);
   }
 
   render() {
 
-    const havePosts = (this.props && this.props.posts && Array.isArray(this.props.posts)) ? true : false;
+    const havePosts = (this.props && this.props.posts &&
+                       Array.isArray(this.props.posts) && this.props.posts.length > 0)
+                    ? true : false;
 
     // set status message to display TODO: state.statusMessage
     let statusMessage = ''
@@ -38,26 +40,30 @@ export class Posts extends Component {
           <ul className="nav"><li>Sort Posts By:</li> <li>Most Recent</li><li>Highest Votes</li></ul>
           </div>
 
+          {/*Categories*/}
           <Categories />
           <hr />
 
+          {/*New Post*/}
           <Link to={`/post/new`} onClick={() => {
-            this.props.onChangeView(`/new`, '')
+            this.props.onChangeView(`/new`, 'newPostIdPlaceholder')
           }}>
             <div><h2>Add New Post</h2><hr /></div>
           </Link>
 
-          <div> {/*post heading*/}
+          <div> {/*posts*/}
             <ol>
               {this.props.posts.map(post => {
                 return (
                   <li key={post.id}>
                     <div>
+
                       <Link to={`/post/${post.id}`} onClick={() => {
                         this.props.onChangeView(`/post/${post.id}`, post.id)
                       }}>
                         <h1>{post.title}</h1>
                       </Link>
+
                       <div>{post.voteScore} Votes | {post.commentCount} Comments</div>
                       <p>{post.category}</p>
                       {/* TODO move Link closing tag to here - after update styles*/}
@@ -78,22 +84,34 @@ export class Posts extends Component {
 function mapDispatchToProps(dispatch){
   return ({
     fetchPosts: (category) => dispatch(fetchPosts(category)),
-    onChangeView: (url, id) => dispatch(changeView({ url, id })),
-    onChangeViewByCategory: (category) => dispatch(changeView({ category })),
+    onChangeView: (url, id) => dispatch(changeView({
+      currentUrl:url,
+      currentId: id
+    })),
+    onChangeViewByCategory: (category) => dispatch(changeView({
+      persistentCategory:category
+    })),
   })
 }
 
 function mapStoreToProps ( store ) {
-  // console.log('store:', store);
+  console.log('store:', store);
 
-  // turn object of post objects into array of post objects (for react mapping)
-  const postIds = Object.keys(store.posts);
-  const posts = postIds.reduce((acc, postId) => {
-    return acc.concat([store.posts[postId]]);
-  }, []);
+  // object to array
+  const posts = Object.keys(store.posts).reduce((acc, postId) => {
+      return acc.concat([store.posts[postId]]);
+    }, []);
+  console.log('posts:', posts)
+
+  const selectedCategoryName = (store && store.viewData
+    && store.viewData.persistentCategory)
+     ? store.viewData.persistentCategory.name
+     : HOME.category.name;
+  // console.log('selectedCategoryName:', selectedCategoryName)
+
   return {
-    posts: posts,
-    categoryName: store.viewData.persistentCategory.name,
+    posts,
+    selectedCategoryName,
   }
 };
 
