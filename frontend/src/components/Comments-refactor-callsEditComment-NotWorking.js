28054f91aff68c73c0293d4396fc928502bab9fe
Sheryl Hohman <sherylhohman@yahoo.com>
2018-02-12 15:54:27 -0800

    refactor: WIP EditComment Modal its own component
        It does not work.
        Comments-refactor-callsEditComment-NotWorking
    	is the WIP version on Comments.js that calls EditComment component
        EditComment-NotWorking-companionTo-Comments-refactor..
    	is "EditComment.js" renamed (to make it obvious it's not currently used)
    	is the WIP EditComment component that Comments.js *would* call - if it worked
        I dunno how to make the modal work, unless its embeded in the component that uses it.  So it's a WIP.
    
    	new file:   components/Comments-refactor-callsEditComment-NotWorking.js
    	deleted:    components/EditComment.js
    	new file:   components/EditComment-NotWorking-companionTo-Comments-refactor.js

---
+++ frontend/src/components/Comments-refactor-callsEditComment-NotWorking.js
@@ -0,0 +1,229 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchComments } from '../store/comments';
+import { upVoteComment, downVoteComment, editComment, deleteComment } from '../store/comments';
+import { dateMonthYear, timeIn12HourFormat, titleCase } from '../utils/helpers';
+import NewComment from './NewComment';
+import EditComment from './EditComment';
+import Modal from 'react-responsive-modal';
+import PropTypes from 'prop-types';
+
+
+export class Comments extends Component {
+
+  static propTypes = {
+    postId: PropTypes.string,//.isRequired,
+    comments: PropTypes.array,
+  }
+
+  state = {
+    isOpenModal: false,
+    id: '',
+    // body: '',
+    // author: '',
+  };
+
+  componentDidMount(){
+    const postId = this.props.postId;
+    // console.log('...in Comments ComponentDidMount, props', this.props);
+    console.log('Comments componentDidMount ..fetching, comments');
+    this.props.fetchComments(postId);
+  }
+
+  // controlledBodyField(e, currentText){
+  //   e.preventDefault();
+  //   this.setState({body: currentText});
+  // }
+  // controlledAuthorField(e, currentText){
+  //   e.preventDefault();
+  //   this.setState({author: titleCase(currentText)});
+  // }
+  onEditComment(comment){
+      this.setState({
+        id: comment.id,
+        // body: comment.body,
+        // author: comment.author,
+        isOpenModal: true,
+      });
+  }
+  // onSave(){
+  //   this.props.updateComment({
+  //     id:this.state.id, //id, //: this.props.id,
+  //     body: this.state.body.trim(),
+  //     author: this.state.author,
+  //     timestamp: Date.now(),   // supposed to update timestamp ?
+  //   });
+  //   this.closeModal();
+  // }
+  onCancel(){
+    this.closeModal();
+  }
+  closeModal(){
+    // this.setState({id:'', body:'', author: '', isOpenModal: false});
+    this.setState({id:'', isOpenModal: false});
+  };
+  onSubmit(e){
+    e.preventDefault();
+    // return false;
+  }
+
+  render() {
+    const { comments, postId } = this.props;
+
+    if (postId === null) {
+      console.log('Comments, are null.');
+      return (
+        <div>Unable to get comments for this post</div>
+      )
+    }
+    if (comments === []) {
+      console.log('There are no comments for this post.');
+      return (
+        <div>Be the first to comment on this post</div>
+      )
+    }
+    // if (comments[0].parentDeleted) {
+    //   return (
+    //     <div>
+    //       <h3> ..Oops! This post has been deleted.</h3>
+    //       <p>No comments to show.</p>
+    //     </div>
+    //   )
+    // }
+
+    return  (
+      <div>
+        <hr />
+        {comments.filter((comment) => !comment.deleted && !comment.parentDeleted)
+          .sort((commentA, commentB) => {
+            if (commentA === commentB) return 0;
+            if (commentA.timestamp < commentB.timestamp) return 1;
+            return -1;
+          })
+          .map((comment) => {
+            return (
+              <li key={comment.id}>
+                <p>{comment.body}</p>
+
+                <div className="vote">
+                  <div
+                    className="comment-up-vote"
+                    onClick={() => {this.props.onUpVoteComment(comment.id)}}
+                  >
+                  </div>
+                  {comment.voteScore}
+                  <div
+                    className="comment-down-vote"
+                    onClick={() => {this.props.onDownVoteComment(comment.id)}}
+                  >
+                  </div>
+                </div>
+
+                <p>by {comment.author},
+                <small> {dateMonthYear(comment.timestamp)} at {timeIn12HourFormat(comment.timestamp)}</small>
+                </p>
+
+                {/*TODO: link styling, change cursor to hand on hover*/}
+                <p className="link">
+                  <span onClick={() => {this.props.onDeleteComment(comment.id)}}> delete </span>
+                   <span className="no-link"> | </span>
+                  <span onClick={(e) => {this.onEditComment(comment)}}> edit </span>
+                </p>
+                <hr />
+              </li>
+            );
+          })
+        }
+
+        <NewComment />
+
+        <div>
+          <Modal little
+            open={this.state.isOpenModal}
+            onClose={() => this.onCancel()}
+            >
+<EditComment
+  comment={this.props.comments[this.state.id]}
+  closeModal={this.closeModal}
+  />
+{/*
+            <div>
+              <form onSubmit={(e)=> {this.onSubmit(e)}}>
+
+                <div>
+                  <p className="field-label-left">Comment:</p>
+                  <textarea
+                    className="comment-body"
+                    type="text"
+                    placeholder="Your insightful comment.."
+                    value={this.state.body}
+                    onChange={ (event) => {this.controlledBodyField(event, event.target.value)} }
+                    rows={'2'}
+                    />
+                </div>
+
+                <div>
+                  <p className="field-label-left">Author:</p>
+                  <input
+                    className="comment-author"
+                    type="text"
+                    placeholder="Your name in lights.."
+                    value={this.state.author}
+                    onChange={ (event) => {this.controlledAuthorField(event, event.target.value)} }
+                    />
+                </div>
+
+                <button
+                  className="on-save"
+                  onClick={() => {this.onSave()}}
+                  >
+                  Save
+                </button>
+                <button
+                  className="on-cancel"
+                  onClick={() => {this.onCancel();
+                }}>
+                  Cancel
+                </button>
+
+              </form>
+            </div>
+*/}
+          </Modal>
+        </div>
+
+      </div>
+    )
+  }
+
+}
+
+function mapDispatchToProps(dispatch){
+  return ({
+    fetchComments:     (postId)    => dispatch(fetchComments(postId)),
+    onUpVoteComment:   (commentId) => dispatch(upVoteComment(commentId)),
+    onDownVoteComment: (commentId) => dispatch(downVoteComment(commentId)),
+    onDeleteComment:   (commentId) => dispatch(deleteComment(commentId)),
+    updateComment:     (editCommentData) => dispatch(editComment(editCommentData)),
+  })
+}
+
+function mapStoreToProps ( store ) {
+
+  const postId = store.viewData.currentId || null;
+
+  //  TODO: if there is already a comments array in store,
+  //    double check that it is for *this* post
+
+  const commentIds = Object.keys(store.comments);
+  const comments = commentIds.reduce((acc, commentId) => {
+    return acc.concat([store.comments[commentId]]);
+  }, []);
+
+  return {
+    postId,
+    comments,
+  }
+};
+
+export default connect(mapStoreToProps, mapDispatchToProps)(Comments);
---
+++ frontend/src/components/EditComment-NotWorking-companionTo-Comments-refactor.js
@@ -0,0 +1,140 @@
+// CURRENTLY THIS CODE IS EMBEDDED IN COMMENTS.js
+// DON'T USE THIS FILE
+// HAVEN'T GOTTEN MODAL TO WORK AS EXTERNAL FILE
+// COMMENTS-refactor-callsEditComment-NotWorking.js is
+//   The WIP version of Comments that *should* work with *this* code
+import React, { Component } from 'react';
+import { editComment } from '../store/comments';
+import { connect } from 'react-redux';
+import { titleCase } from '../utils/helpers';
+// import PropTypes from 'prop-types';
+
+// NOT USING - NOT WORKING AS SEPARATE COMPONENT !
+// TO BE USED IN CONJUNCTION WITH THE CODE IN  Comments-tryingToSeparate...
+export class EditComment extends Component {
+  state = {
+    // isOpenModal: false,
+    // id: '',
+    body: '',
+    author: '',
+  };
+
+  componentDidMount(){
+    console.log('cDM, props:', this.props);
+    if (this.props && this.props.comment){
+    // if (this.props && this.props.comment){
+      // this.setState({ body: this.props.comment.body });
+      this.setState({
+        body: this.props.comment.body,
+        author: this.props.comment.author
+      });
+    }
+  }
+
+  controlledBodyField(e, currentText){
+    e.preventDefault();
+    this.setState({body: currentText});
+  }
+  controlledAuthorField(e, currentText){
+    e.preventDefault();
+    this.setState({author: titleCase(currentText)});
+  }
+
+  onSave(){
+    this.props.updateComment({
+      id:this.props.comment.id, //id, //: this.props.id,
+      body: this.state.body.trim(),
+      author: this.state.author,
+      timestamp: Date.now(),   // supposed to update timestamp ?
+    });
+    this.props.closeModal();
+  }
+  onCancel(){
+    this.props.closeModal();
+  }
+  // closeModal(){
+  //   this.setState({id:'', body:'', author: '', isOpenModal: false});
+  // };
+  onSubmit(e){
+    e.preventDefault();
+    // return false;
+  }
+
+  render() {
+
+    return (
+            <div>
+              <form onSubmit={(e)=> {this.onSubmit(e)}}>
+
+                <div>
+                  <p className="field-label-left">Comment:</p>
+                  <textarea
+                    className="comment-body"
+                    type="text"
+                    placeholder="Your insightful comment.."
+                    value={this.state.body}
+                    onChange={ (event) => {this.controlledBodyField(event, event.target.value)} }
+                    rows={'2'}
+                    />
+                </div>
+
+                <div>
+                  <p className="field-label-left">Author:</p>
+                  <input
+                    className="comment-author"
+                    type="text"
+                    placeholder="Your name in lights.."
+                    value={this.state.author}
+                    onChange={ (event) => {this.controlledAuthorField(event, event.target.value)} }
+                    />
+                </div>
+
+                <button
+                  className="on-save"
+                  onClick={() => {this.onSave()}}
+                  >
+                  Save
+                </button>
+                <button
+                  className="on-cancel"
+                  onClick={() => {this.onCancel();
+                }}>
+                  Cancel
+                </button>
+
+              </form>
+            </div>
+    );
+  }
+
+}
+
+
+function mapDispatchToProps(dispatch){
+  return ({
+    saveComment: (editedCommentData) => {
+      dispatch(editComment(editedCommentData))
+    },
+    // closeModal: () => dispatch(this.props.closeModal());
+  })
+}
+
+function mapStoreToProps (store, ownProps) {
+  console.log('ownProps:', ownProps);
+  console.log('store.comments[ownProps.comment]:', store.comments[ownProps.comment]);
+  // console.log('store.comments[ownProps.commentId]:', store.comments[ownProps.commentId]);
+  const id = ownProps.comment.id
+  return {
+    // comment: store.comments[ownProps.commentId],
+    // body: store.comments[ownProps.commentId].body,
+    // id: ownProps.commentId,
+    id,
+    body: store.comments[id].body,
+    author: store.comments[id].author,
+  }
+};
+
+
+// export default connect(mapStoreToProps, mapDispatchToProps)(EditComment);
+export default connect(null, mapDispatchToProps)(EditComment);
+
