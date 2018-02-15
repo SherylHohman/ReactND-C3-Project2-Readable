fcb88a8f435603d698ae9c0e41b0ea5331326c1e
Sheryl Hohman <sherylhohman@yahoo.com>
2018-02-14 16:10:53 -0800

    refactor: uri replaces dependence on viewData
        Edit Post <Link> no longer needs an onClick handler to set the
    	viewData url for "editing" a post. Instead, since <Link to="">
    	changes the url, the postId is read from the url when
    	the component mounts.
        changeView function ONLY accepts a uri parameter.
    	deleted the versions that accepted a category object,
    	or url, currentId
        (slowly removing the necessity for viewData {url, currentId} fields)
    
        - modified:   components/Post.js

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -27,18 +27,17 @@ export class Post extends Component {
     }
 
     if (this.props.uri){
-      this.props.updateLocation(this.props.uri)
+      console.log('__Post cDM calling changeView, this.props.uri', this.props.uri);
+      this.props.changeView(this.props.uri)
+    }
+    else {
+      console.log('__Post cDM NOT calling changeView, this.props.uri', this.props.uri);
+
     }
   }
 
   componentWillReceiveProps(nextProps){
-    // console.log('__Post, nextProps:', nextProps);
-
-    // if (nextProps.uri && (nextProps.uri.url) &&
-    //     nextProps.uri !== this.props.uri   // shallow check
-    //    ){
-    //     this.updateLocation(nextProps.uri)
-    // }
+    // maybe check for 404 (deleted postId, bad postId), or network error here ?
   }
 
   onDelete(postId){
@@ -47,11 +46,12 @@ export class Post extends Component {
     // Link redirects to category that this post previously appeared
     // viewData musht store this same info to keep in synch
     // Not Dry :/ - TODO: read and parse data from URL routes
-    const category = {
-      name: this.props.post.category,
-      path: this.props.categoryPath,
-    }
-    this.props.changeViewByCategory(category);
+
+    // const category = {
+    //   name: this.props.post.category,
+    //   path: this.props.categoryPath,
+    // }
+    // this.props.changeViewByCategory(category);
   }
 
   render(){
@@ -59,10 +59,12 @@ export class Post extends Component {
     const props = this.props;
     if (!this.props){console.log('Post props is undefined');}
 
-    // TODO: show error message if  error fetching post: 404, retry, postDeleted..
+    // TODO: show error message if error fetching post:
+    //  post Deleted, bad PostId, or network error
+    //  options: 404, retry, homePage
 
     if (!this.props.post) {
-      console.log('Post: post wasn\'t present in props, do I have the postID?:', this.props.postId);
+      // console.log('Post: post wasn\'t present in props, do I have the postID?:', this.props.postId);
       return (
         <div>
           <p>looking for your Post:</p>
@@ -74,17 +76,18 @@ export class Post extends Component {
     const {title, body, voteScore, commentCount, author, timestamp } = this.props.post;
 
     // disambiguate:
-    // category is an {name, path} on categories, yet
+    // category is an {name, path} object on categories, yet
     // category on a post refers to a category.name
     const categoryName = this.props.post.category;
+    const categoryPath = this.props.categoryPath;
 
     return (
       <div>
         <div>
             <Link
               to={`/post/${postId}`}
-              onClick={() => {this.props.onChangeView(`/post/${postId}`, postId)
-            }}>
+                /*onClick={() => {this.props.onChangeView(`/post/${postId}`, postId)}*/
+            >
               <h2>{title}</h2>
             </Link>
 
@@ -111,15 +114,15 @@ export class Post extends Component {
 
             <div>
               <Link
-                to={`/category/${this.props.categoryPath}`}
+                to={`/category/${categoryPath}`}
                 onClick={() => {this.onDelete(postId)}}
               >
                 Delete Post
               </Link>
                 <Link
                   to={`/post/${postId}/edit`}
-                  onClick={() => {this.props.onChangeView(`/post/${postId}/edit`, postId)
-                }}>
+                    /*onClick={() => {this.props.onChangeView(`/post/${postId}/edit`, postId)}*/
+                >
                   Edit Post
                 </Link>
             </div>
@@ -141,7 +144,8 @@ Post.propTypes = {
 
 function mapDispatchToProps(dispatch){
   return ({
-    onChangeView:  (url, id) => dispatch(changeView({ currentUrl:url, currentId:id })),
+    changeView:  (uri) => dispatch(changeView({ uri })),
+    // onChangeView:  (url, id) => dispatch(changeView({ currentUrl:url, currentId:id })),
     onUpVotePost:   (postId) => dispatch(upVotePost(postId)),
     onDownVotePost: (postId) => dispatch(downVotePost(postId)),
 
@@ -158,16 +162,22 @@ function mapStoreToProps (store, ownProps) {
   // console.log('Post ownProps:', ownProps);
 
   const uri = getUri(ownProps.routerInfo) || null;
+  const postId = uri.postId  || null;
+  // so can redirect to Post's (former) category when deleting the post
+  const categoryPath = (store.categories[postId] && store.categories[postId].path) || null;
+
 
   return {
-    postId: uri.postId,
-    post:   store.posts[uri.postId],
+    postId,
+    post: store.posts[postId],
+    categoryPath,
 
-    // uri: {url, route, params{postId, categoryPath}}
-    // uri: {currentId, postId, categoryPath}  //currentId replaces postId & categoryPath
+    // uri: {url, route, params, currentId}
+    // uri.params is either: postId OR categoryPath  //currentId replaces postId & categoryPath
     uri,
 
-    history: ownProps.routerInfo.history  // so can history.push if have bad postId
+    // so don't have to refactor former history.push references (if have bad postId)
+    history: ownProps.routerInfo.history
   }
 };
 
