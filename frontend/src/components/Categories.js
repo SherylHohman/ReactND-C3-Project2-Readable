9a97527eed95323334153f81d72bed3ecadffbb1
Sheryl Hohman <sherylhohman@yahoo.com>
2018-02-16 00:48:28 -0800

    refactor: uses only url to fetch posts by category
        Also no longer meed onClick for category <Links>, since the
    	to="" field supplies the new urlPath.
        Then, when Posts sees the new url, *it* will fetch the new posts
    	-- and update store with the new url, and category info.
        uri is the structure I'm using for url encoded data
        Code/Comment cleanup.
    
    	- modified:   components/Categories.js

--- frontend/src/components/Categories.js
+++ frontend/src/components/Categories.js
@@ -9,47 +9,23 @@ import { changeView, HOME, DEFAULT_SORT_BY, getUri } from '../store/viewData';
 export class Categories extends Component {
 
   componentDidMount() {
-    // may need to move this to App.js
     this.props.fetchCategories();
-    console.log('Categories componentDidMount ..fetching, categories');
+    // console.log('Categories componentDidMount ..fetching, categories');
 
     if (this.props.uri){
-      console.log('__Categories cDM calling changeView, this.props.uri', this.props.uri);
+      // console.log('Categories cDM calling changeView, this.props.uri', this.props.uri);
       this.props.changeView(this.props.uri)
     }
     else {
-      console.log('__Categories cDM NOT calling changeView, this.props.uri', this.props.uri);
+      // console.log('Categories cDM NOT calling changeView, this.props.uri', this.props.uri);
     }
-  }
-
-  getCategoryFromName(categoryName){
-    if (!categoryName){
-      return HOME.category;  // home page, show all categories
-    } else {
-      return this.props.categories.find((category) => {
-        return category.name === categoryName;
-      });
-    }
-  }
-
-  // TODO: Do I fetch POSTS based on changing uri instead ??
-  onSelectCategory(categoryPath){
-  //   const category = this.getCategoryFromName(categoryPath);
-  //   this.props.getPosts(category.path);
-
-    // const categoryPath = this.props.categories[categoryName].path || HOME.category.path;
-    this.props.getPosts(categoryPath);
   }
 
   render() {
 
     const isExactPath = () => {
-      // TEMP
-      if (!this.props.history || !this.props.history.location || !this.props.history.location.pathname){
-        return false;
-      }
-
-      return (('/' + this.props.category.path) === this.props.history.location.pathname);
+      return (this.props.uri && this.props.uri.url) &&
+             (this.props.uri.url === HOME.url);
     }
 
     return (
@@ -60,7 +36,6 @@ export class Categories extends Component {
                 <li className="no-link"> Category: </li>
                 <NavLink key="all-categories-makeSureThisKeyIsUnique"
                       to={HOME.category.path}
-                      onClick={() => {this.onSelectCategory(HOME.category.path)}}
                       activeClassName={"selected"}
                       isActive={isExactPath}
                       >
@@ -72,7 +47,6 @@ export class Categories extends Component {
                   return (
                     <NavLink key={category.name}
                       to={`/category/${category.path}`}
-                      onClick={() => {this.onSelectCategory(category.path)}}
                       activeClassName="selected"
                           >{category.name}
                       <li key={category.name}></li>
@@ -95,8 +69,6 @@ function mapDispatchToProps(dispatch){
   return ({
     fetchCategories: () => dispatch(fetchCategories()),
     getPosts: (category) => dispatch(fetchPosts(category)),
-    // changeView: (id, url) => dispatch(changeView({ currentId: id, currentUrl: url })),
-    // changeViewByCategory: (category) => dispatch(changeView(category)),
     changeView: (uri) => dispatch(changeView({ uri })),
   })
 }
@@ -104,27 +76,20 @@ function mapDispatchToProps(dispatch){
 function mapStoreToProps (store, ownProps) {
   // console.log('store.categories:', store.categories)
   // console.log('Categories, ownProps:', ownProps)
+
+  // so don't have to refactor former history references
+  const history = (ownProps.routerInfo && ownProps.routerInfo.history )|| null;
+
   const categoriesArray = Object.keys(store.categories).reduce((acc, categoryKey) => {
     return acc.concat([store.categories[categoryKey]]);
   }, []);
 
-  // so don't have to refactor former history.push references
-  const history = (ownProps.routerInfo && ownProps.routerInfo.history )|| null;
-
   const uri = getUri(ownProps.routerInfo) || null;
-  // console.log('uri:', uri);
-  // uri.currentId === uri.categoryPath === categories[uri.categoryPath].path
-  const category = (uri.currentId && store.categories &&
-                    store.categories[uri.currentId]) || HOME.category;
 
   return {
-      categories: categoriesArray || null,
-      // category: store.viewData.category || HOME.category,
-      sortBy:   store.viewData.sortBy   || DEFAULT_SORT_BY,
-
+      categories: categoriesArray   || null,
+      sortBy: store.viewData.sortBy || DEFAULT_SORT_BY,
       uri,
-      currentCategory: category,  //TEMP
-      currentCategoryPath: category.path,  //TEMP
   }
 };
 
