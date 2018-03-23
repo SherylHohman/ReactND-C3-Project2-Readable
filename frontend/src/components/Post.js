a3b5f21df3ee6797b00d8e5a5305212a1a52f14f
Sheryl Hohman <sherylhohman@yahoo.com>
2018-03-23 12:23:06 -0700

    style: refactor: edit/delete comments/logging
      refactor: some functions that no longer need logging could be refactored
        to be more concise, since they no longer need logging constants.
    
      style: edit/delete comments and logging.  Allows for refactor above.
    
      - modified:   components/Post.js
    
        On branch working_branch_review2

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -35,27 +35,14 @@ export class Post extends Component {
     }
   }
 
-  // componentWillReceiveProps(nextProps){
-  //   if (nextProps.postId && nextProps.postId !== this.props.postId){
-  //     //   console.log('Post.wRP ..fetching, post for postId:', this.props.postId);
-  //     this.props.fetchPost(this.props.postId);
-  //   }
-  //   if (nextProps.routerProps &&
-  //      (nextProps.routerProps !== this.props.routerProps)){
-  //     this.props.changeView(this.props.routerProps)
-  //   }
-  // }
-
   disableClick(e){
     e.preventDefault();
   }
 
   render(){
-
     // console.log('Post.render fetchStatus', this.props.fetchStatus);
     const postId = this.props.postId;
 
-    // made this DRY
     if (!this.props.post) {
       // loading "spinner", fetch failure, or 404
       return (
@@ -68,158 +55,60 @@ export class Post extends Component {
       );
     }
 
-    // only set these constants After early return for non-existant this.props.post (isLoading, or isFetchFailure)
+    // Set these constants After early return for non-existant post
     const {title, body, voteScore, commentCount, author, timestamp } = this.props.post;
-    const categoryName = this.props.post.category;
+
     // disambiguate category --> categoryName:
     // category is an {name, path} object on categories, yet
     // category on a post refers to a category.name
-
-    // const makePostUrl = () => {
-    //   if (!this.props || !this.props.postId || !this.props.categoryPath){
-    //     console.log('Post.render.makePostUrl Missing "postId" or "categoryPath", props:', this.props);
-    //     return null;
-    //   }
-    //   const routeName = 'post';
-    //   const params = {
-    //     postId: this.props.postId,
-    //     categoryPath: this.props.categoryPath,
-    //   };
-    //   console.log('Post.render.makePostUrl, "post", params:', params);
-    //   const url = computeUrlFromParamsAndRouteName(params, routeName);
-    //   console.log('Post.render.makePostUrl, url', url);
-    //   return url;
-    // }
-    // const makeCategoryUrl = () => {
-    //   if (!this.props || !this.props.categoryPath){
-    //     console.log('Post.render.makeCategoryUrl Missing "categoryPath", props:', this.props);
-    //     return null;
-    //   }
-    //   const routeName = 'category';
-    //   const params = {
-    //     categoryPath: this.props.categoryPath,
-    //   };
-    //   console.log('Post.render.makeCategoryUrl, "post", params:', params);
-    //   const url = computeUrlFromParamsAndRouteName(params, routeName);
-    //   console.log('Post.render.makeCategoryUrl, url', url);
-    //   return url;
-    // }
-    // const makeEditPostUrl = () => {
-    //   const routeName = 'editPost';
-    //   const params = {};
-    //   console.log('Post.render.makeEditPostUrl, "post", params:', params);
-    //   const url = computeUrlFromParamsAndRouteName(params, routeName);
-    //   console.log('Post.render.makeEditPostUrl, url', url);
-    //   return url;
-    // }
+    const categoryName = this.props.post.category;
 
     const makeUrl = (routeName) => {
       console.log('Post.render.makeUrl, routeName:', routeName, ', props:', this.props);
       let params = {};
-      let url = null;
       switch (routeName){
         case 'editPost':
           if (!this.props || !this.props.postId){
-            console.log('Post.render.makePostUrl Missing "postId", props:', this.props);
+            console.log('ERROR: Post.render.makePostUrl Missing "postId", props:', this.props);
             return null;
           }
           params = {
             postId: this.props.postId,
           }
-          // return computeUrlFromParamsAndRouteName(params, routeName);
-          console.log('Post.render.makeUrl,', routeName, ', params:', params);
-          url = computeUrlFromParamsAndRouteName(params, routeName);
-          console.log('Post.render.makeUrl, url', url);
-          return url;
+          return computeUrlFromParamsAndRouteName(params, routeName);
 
         case 'category':
           if (!this.props || !this.props.categoryPath){
-            console.log('Post.render.makeCategoryUrl Missing "categoryPath", props:', this.props);
+            console.log('ERROR: Post.render.makeCategoryUrl Missing "categoryPath", props:', this.props);
             return null;
           }
           params = {
             categoryPath: this.props.categoryPath,
           }
-          // return computeUrlFromParamsAndRouteName(params, routeName);
-          console.log('Post.render.makeUrl,', routeName, ', params:', params);
-          url = computeUrlFromParamsAndRouteName(params, routeName);
-          console.log('Post.render.makeUrl, url', url);
-          return url;
+          return computeUrlFromParamsAndRouteName(params, routeName);
 
         case 'post':
           if (!this.props || !this.props.postId || !this.props.categoryPath){
-            console.log('Post.render.makePostUrl Missing "postId" or "categoryPath", props:', this.props);
+            console.log('ERROR: Post.render.makePostUrl Missing "postId" or "categoryPath", props:', this.props);
             return null;
           }
           params = {
             postId: this.props.postId,
             categoryPath: this.props.categoryPath,
           }
-          // return computeUrlFromParamsAndRouteName(params, routeName);
-          console.log('Post.render.makeUrl,', routeName, ', params:', params);
-          url = computeUrlFromParamsAndRouteName(params, routeName);
-          console.log('Post.render.makeUrl, url', url);
-          return url;
+          return computeUrlFromParamsAndRouteName(params, routeName);
 
         default:
-          console.log('Post.render.makeUrl, defaulting. routeName:', routeName);
+          console.log('ERROR: Post.render.makeUrl, defaulting. Unknown routeName:', routeName);
           return null;
       }
     }
 
-// const makeUrl = (routeName) => {
-//       console.log('Post.render.makeUrl, routeName:', routeName, ', props:', this.props);
-//       let params = {};
-//       let url = null;
-//       switch (routeName){
-//         case 'post':
-//           if (!this.props || !this.props.postId || !this.props.categoryPath){
-//             console.log('Post.render.makePostUrl Missing "postId" or "categoryPath", props:', this.props);
-//             return null;
-//           }
-//           params = {
-//             postId: this.props.postId,
-//             categoryPath: this.props.categoryPath,
-//           }
-//           // return computeUrlFromParamsAndRouteName(params, routeName);
-//         case 'category':
-//           if (!this.props || !this.props.categoryPath){
-//             console.log('Post.render.makeCategoryUrl Missing "categoryPath", props:', this.props);
-//             return null;
-//           }
-//           params = {
-//             categoryPath: this.props.categoryPath,
-//           }
-//           // return computeUrlFromParamsAndRouteName(params, routeName);
-//         case 'editPost':
-//           // return computeUrlFromParamsAndRouteName(params, routeName);
-//           if (!this.props || !this.props.postId){
-//             console.log('Post.render.makePostUrl Missing "postId", props:', this.props);
-//             return null;
-//           }
-//           params = {
-//             postId: this.props.postId,
-//           }
-//           console.log('Post.render.makeUrl,', routeName, ', params:', params);
-//           url = computeUrlFromParamsAndRouteName(params, routeName);
-//           console.log('Post.render.makeUrl, url', url);
-//           return url;
-
-//         default:
-//           console.log('Post.render.makeUrl, defaulting. routeName:', routeName);
-//           return null;
-//       }
-//     }
-
-
-
-    console.log('Posts.render, props:', this.props);
+    // console.log('Posts.render, props:', this.props);
     return (
       <div>
         <div>
-            <Link /*to={`${ROUTES.post.base}${categoryName}/${postId}`}*/
-                  /*to={makePostUrl()}*/
-                  to={makeUrl('post')}
+            <Link to={makeUrl('post')}
                   onClick={this.disableClick}
                   style={{cursor:"default"}}
               >
@@ -248,18 +137,13 @@ export class Post extends Component {
             <p>category: {titleCase(categoryName)} | by: {author}, {dateMonthYear(timestamp)}</p>
 
             <div>
-              <Link
-                    /*to={`${ROUTES.category.base}${this.props.categoryPath}`}*/
-                    /*to={makeCategoryUrl()}*/
-                    to={makeUrl('category')}
+              <Link to={makeUrl('category')}
                     onClick={() => {this.props.deletePost(postId)}}
                 >
                 Delete Post
               </Link>
 
-              <Link /*to={`${ROUTES.editPost.base}${postId}`}*/
-                    /*to={makeEditPostUrl()}*/
-                    to={makeUrl('editPost')}
+              <Link to={makeUrl('editPost')}
                 >
                 Edit Post
               </Link>
@@ -296,38 +180,36 @@ function mapStoreToProps (store, ownProps) {
   // console.log('Post store:', store);
   // console.log('Post ownProps:', ownProps);
 
-  //  Either call getLoc from routerProps to get most up-to-date postId
-  //    here, and call fetch from componentDidMount
-  // NOPE: DOESNT WORK: OR get loc directly from store, let componentDidMount update loc
-  //      using routerProps, but WAIT to call Fetch from componentWillReceiveProps
-  //      as postId may change in the process, and we don't want to make an
-  //      unnecessary network request on the postId, only to immediately make another
-  //      request with the correct postId.
-  //  Not sure which is considered best practice.
-  //  It does not seem quite right to not use store to retrieve loc,
-  //    Then again, router *is* the source of truth, and I'm simply keeping store
-  //    in synch with router..
+  //  call getLoc from routerProps RATHER THAN store.viewData.loc
+  //    to get most up-to-date postId here
+  //    rather than wait for cDM to call updateView and store to asynch update
+  //    store.viewData.loc (url,postId,categoryPath,..) to the current Page
+  //    based on routerProps info.
+  //  Optimization:
+  //    uses routerProps to get infor for CURRENT browser Url
+  //    rather than the one saved to store
+  //    This is because routerProps is the source of truth for this component
+  //    (since component ONLY renders on EXACT match)
+  //  This is because at componentDidMount, store has the url of the PREVIOUS
+  //    page.  cDM THEN calls changeView to UPDATE store to make it in synch
+  //    with the current browser url..
+  //  Which would then re-trigger a re-render with the updated (correct) url.
+  //  Instead, set component props using router's url, so loc doesn't change
+  //    after store's url is updated, cuz it *already* had the new value.
 
   // const loc = store.viewData.loc;
   const loc = getLoc(ownProps.routerProps) || null;
-  const postId = loc.postId;
+  const postId = loc.postId;   // *always* Exists on *this* page/component/route
 
   const post = getPostsAsObjects(store)[postId] || null;
   const fetchStatus = getFetchStatus(store);
-  // console.log('Post.mSTP fetchStatus:', fetchStatus);
-  // (isLoading, isFetchFailure, errorMessage)
 
-  // get his post's categoryPath so can redirect to Post's (former) category
-  //   when deleting the post. Here I have access to store.categories, which
-  //   is only used as an intermediate step for this one-time calc.
+  // So can easily redirect to Post's (former) category when deleting the post.
   const categoryPath = loc.categoryPath || null;
-  // const categoryName = (post && post.category) || null;
-  // const category     = categoryName && store.categories[categoryName];
-  // const categoryPath = (category    && store.categories[categoryName].path) || null;
 
   return {
-    postId,
     post,
+    postId,
     categoryPath,
     fetchStatus,
   }
