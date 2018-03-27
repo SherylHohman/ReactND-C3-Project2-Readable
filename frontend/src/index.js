660545ec28edf10b8383f751543020c1b8668dd5
Sheryl Hohman <sherylhohman@yahoo.com>
2018-03-27 13:45:34 -0700

    feat: add redux-logger when ENV='development'
      style: also re-ordered imports and added/removed comments
    
    	- modified:   index.js
    
        On branch working_branch_s3

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,19 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';    // not using..yet anyway (see Notes) below
+import registerServiceWorker from './registerServiceWorker';
+
+//  Middlewares
+import thunk  from 'redux-thunk';
+import logger from 'redux-logger'
+
+//  App
 import rootReducer from './store/index.js';
 import App from './components/App';
 import './index.css';
-import registerServiceWorker from './registerServiceWorker';
 
 const composeEnhancers =
   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV === `development`) {
+  // https://stackoverflow.com/a/48396154/5411817 - 'development' is set by
+  //  a webpack script at node_modules react-scripts/scripts/config/env.js,
+  //  as per create-react-app (unless decide to `eject` it)
+  const { logger } = require(`redux-logger`);
+  middlewares.push(logger);
+  // Note: logger must be the last middleware in chain,
+  //  otherwise it will log thunk and promise, not actual actions (#20).
+}
+
 const store = createStore(
   rootReducer,   // preloaded state
   composeEnhancers(
-    applyMiddleware(thunk)
+    applyMiddleware(...middlewares)
   )
 );
 
